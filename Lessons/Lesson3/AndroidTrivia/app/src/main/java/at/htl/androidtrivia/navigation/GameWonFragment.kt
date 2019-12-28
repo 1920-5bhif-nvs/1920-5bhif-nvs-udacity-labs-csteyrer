829d7826ec13a1cc40e9de2444ae623cbf4cb627
Christian Steyrer <christian.steyrer@hotmail.com>
2019-12-28 14:55:31 +0100

    Steyrer / 2019-12-28 / Lesson3 / Adding Sharing with ShareCompat

--- Lessons/Lesson3/AndroidTrivia/app/src/main/java/at/htl/androidtrivia/navigation/GameWonFragment.kt
+++ Lessons/Lesson3/AndroidTrivia/app/src/main/java/at/htl/androidtrivia/navigation/GameWonFragment.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.*
 import android.widget.Toast
+import androidx.core.app.ShareCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
@@ -31,15 +32,23 @@ class GameWonFragment : Fragment() {
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
         inflater.inflate(R.menu.winner_menu, menu)
+
+        if (null == getShareIntent().resolveActivity(activity!!.packageManager)) {
+            menu.findItem(R.id.share)?.setVisible(false)
+        }
     }
 
     private fun getShareIntent() : Intent {
         var args = GameWonFragmentArgs.fromBundle(arguments!!)
-        val shareIntent = Intent(Intent.ACTION_SEND)
-        shareIntent.setType("text/plain")
-                .putExtra(Intent.EXTRA_TEXT,
-                        getString(R.string.share_success_text, args.numCorrect,
-                        args.numQuestions))
+//        val shareIntent = Intent(Intent.ACTION_SEND)
+//        shareIntent.setType("text/plain")
+//                .putExtra(Intent.EXTRA_TEXT,
+//                        getString(R.string.share_success_text, args.numCorrect,
+//                        args.numQuestions))
+        val shareIntent = ShareCompat.IntentBuilder.from(activity)
+                .setText(getString(R.string.share_success_text, args.numCorrect, args.numQuestions))
+                .setType("text/plain")
+                .intent
         return shareIntent
     }
 
