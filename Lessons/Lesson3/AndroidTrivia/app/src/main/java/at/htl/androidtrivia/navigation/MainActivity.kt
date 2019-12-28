e0a88b66221d4b4711bd627df8a8605c1245e914
Christian Steyrer <christian.steyrer@hotmail.com>
2019-12-28 13:10:15 +0100

    Steyrer / 2019-12-28 / Lesson3 / Adding Safe Arguments

--- Lessons/Lesson3/AndroidTrivia/app/src/main/java/at/htl/androidtrivia/navigation/MainActivity.kt
+++ Lessons/Lesson3/AndroidTrivia/app/src/main/java/at/htl/androidtrivia/navigation/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.R
