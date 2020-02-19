5fbf10f3f57860783ac3ec1e21c242f767aa28a4
Christian Steyrer <christian.steyrer@hotmail.com>
2020-02-19 17:32:03 +0100

    Steyrer / 2020-02-19 / Lesson7 / Add a Header

--- Lessons/Lesson7/SleepTrackerRecyclerView/app/src/main/java/at/htl/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ Lessons/Lesson7/SleepTrackerRecyclerView/app/src/main/java/at/htl/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -64,9 +64,15 @@ class SleepTrackerFragment : Fragment() {
         binding.sleepTrackerViewModel = sleepTrackerViewModel
 
         val manager = GridLayoutManager(activity, 3)
+        manager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+            override fun getSpanSize(position: Int) =  when (position) {
+                0 -> 3
+                else -> 1
+            }
+        }
         binding.sleepList.layoutManager = manager
 
-        val adapter = SleepNightAdapter(SleepNightAdapter.SleepNightListener { nightId ->
+        val adapter = SleepNightAdapter(SleepNightListener { nightId ->
             sleepTrackerViewModel.onSleepNightClicked(nightId)
         })
         binding.sleepList.adapter = adapter
@@ -80,7 +86,7 @@ class SleepTrackerFragment : Fragment() {
         })
 
         sleepTrackerViewModel.nights.observe(viewLifecycleOwner, Observer {
-            adapter.submitList(it)
+            adapter.addHeaderAndSubmitList(it)
         })
 
         binding.setLifecycleOwner(this)
