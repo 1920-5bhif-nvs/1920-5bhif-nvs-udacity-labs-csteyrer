065bacdcdb53d6e94dcd853879946d7019820b48
Christian Steyrer <christian.steyrer@hotmail.com>
2020-02-20 11:00:40 +0100

    Steyrer / 2020-02-20 / Lesson8 / Display an Internet Image

--- Lessons/Lesson8/MarsRealEstate/app/src/main/java/at/htl/marsrealestate/overview/OverviewFragment.kt
+++ Lessons/Lesson8/MarsRealEstate/app/src/main/java/at/htl/marsrealestate/overview/OverviewFragment.kt
@@ -23,6 +23,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import at.htl.marsrealestate.R
 import at.htl.marsrealestate.databinding.FragmentOverviewBinding
+import at.htl.marsrealestate.databinding.GridViewItemBinding
 
 /**
  * This fragment shows the the status of the Mars real-estate web services transaction.
@@ -42,7 +43,7 @@ class OverviewFragment : Fragment() {
      */
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        val binding = FragmentOverviewBinding.inflate(inflater)
+        val binding = GridViewItemBinding.inflate(inflater)
 
         // Allows Data Binding to Observe LiveData with the lifecycle of this Fragment
         binding.setLifecycleOwner(this)
